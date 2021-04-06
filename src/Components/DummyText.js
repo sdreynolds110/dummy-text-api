c567e03d0e8f739c6a115bd9c42591ae76ef9d97
Stephen Reynolds <sdreynolds110@gmail.com>
2021-04-06 15:29:05 -0400

    added loading message between calls

--- src/Components/DummyText.js
+++ src/Components/DummyText.js
@@ -4,28 +4,64 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            paragraph: 5
+            isLoading: true,
+            paragraphs: 1,
+            results: []
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(ev) {
-        this.setState({paragraphs: ev.target.value});
+    componentDidMount() {
+        this.fetchText();
     }
 
+    fetchText() {
+        this.setState({isLoading: true});
+        
+        const url ='https://baconipsum.com/api/?type=meat-and-filler&paras=' + this.state.paragraphs;
+        fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then((json) => {
+            this.setState({
+                results: json,
+                isLoading: false
+            })
+        });
+    }
+
+    handleChange(ev) {
+        this.setState({paragraphs: ev.target.value}, () => {
+            this.fetchText();
+        });
+    }
+
+    displayResults() {
+        if (this.state.isLoading) {
+            return <p>Loading...</p>
+        } else {
+            return this.state.results.map((paragraphText, index) => {
+                return <p key={index}>{paragraphText}</p>
+            })
+        }
+    }
 
     render() {
         return <div id="dummy-container">
             <h1>Dummy Text Generator</h1>
-            <div id="dummy-text-result"></div>
             <div id="dummy-text-controls">
                 <h2>Realy Time Options:</h2>
                 <p>
                     Paragraphs:
                 </p>
                 <p>
-                    <input type="number" value={this.state.paragraphs} onChange={this.handleChange} />
+                    <input type="number" value={this.state.paragraphs} min='1' onChange={this.handleChange} />
+                
                 </p>
+            </div>
+            <div id="dummy-text-result">
+                {this.displayResults()}
             </div>
         </div>
   }
